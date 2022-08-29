e51998a5bbb816b981259ef16114912e3c48dbd1
romantafelski <romantafelski18@gmail.com>
2022-08-29 15:15:21 -0400

    jd;sflk

--- models/memes.js
+++ models/memes.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose")
 
 const frontSchema = new mongoose.Schema ({
-    meme: string 
-    
+    meme: string
 })
 
 const Schema = mongoose.model("Schema", frontSchema)
