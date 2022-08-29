a557bec91ba24f20a8a59559dbc0b7a06a6f3489
romantafelski <romantafelski18@gmail.com>
2022-08-29 15:20:13 -0400

    jldf;a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Schema = require('../models/Schema.js')
+const Schema = require('./models/Schema.js')
 require('dotenv').config()
 
 const app = express();
