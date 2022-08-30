5a57a91bea3292d72b913b03996f11c3920a0e28
romantafelski <romantafelski18@gmail.com>
2022-08-30 09:33:00 -0400

    kjldsf

--- blog-front/src/App.js
+++ blog-front/src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from "react"
+import axios from 'axios';
 
 const App = () => {
   
   let [memes, setMemes] = useState([])
-  let [newPost, setPost] = useState("")
+  let [newTitle, setTitle] = useState("")
+  let [newText,setNewText] = useState("")
+  let [newImage, setNewImage] = useState("")
+
+  handleChangeTitle = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  handleTextChange = (event) => {
+    setNewText(event.target.value)
+  }
+
+    handleImageChange = (event) => {      
+      setNewImage(event.target.value)
+    }
 
   const handleMemeCreation = (event) => {
     event.preventDefault()
     axios.post(`https://project-3-front.herokuapp.com/memes/${memes._id}`, 
     {
-      meme: newMeme
+      title: newTitle,
+      text: newText,
+      image: newImage
     }
     ).then(() => {
       axios.get("https://project-3-front.herokuapp.com/memes").then((response=> {
@@ -20,18 +37,42 @@ const App = () => {
     })
   }
 
+  
   useEffect(() => {
-    axios.get('https://project-3-front.herokuapp.com/')
+    axios.get('https://project-3-front.herokuapp.com/memes')
     .then((response) => {
       setMeme(response.data)
     })
   }, [])
+  
+  const handleMemesDelete = (memes) => {
+    axios.delete(`https://project-3-front.herokuapp.com/memes${memes.id}`).then(() => {
+      axios.get("https://project-3-front.herokuapp.com/memes").then((response) => {
+        setMemes(response.data)
+      })
+    })
+  }
+
+  const editMemes = (memes) => {
+    axios.put(`https://project-3-front.herokuapp.com/${memes._id}`, 
+    {
+      title: newTitle,
+      text: newText,
+      image: newImage
+    }).then(() => {
+      axios.get("https://project-3-front.herokuapp.com/memes").then((response) => {
+        setMemes(response.data)
+      })
+    })
+  }
 
   return (
     <div>
     <h1>Hello World</h1>
     <form onSubmit={handleMemeCreation}>
-    Meme: <input type="text"/>
+    Title: <input type="text"/>
+    Text: <input type="text"/>
+    Image: <input type="text"/>
     </form>
     </div>
   )
